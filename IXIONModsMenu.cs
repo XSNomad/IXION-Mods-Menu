5c5a0c15482b665d6b1f7cadd89a419ebd079e85
Olympia-H <133029122+Olympia-H@users.noreply.github.com>
2023-05-21 05:18:48 +1000

    0.2
    * Added mods menu to in-game options
    * Added support for mods to allow in-game disabling through menu

--- IXIONModsMenu.cs
+++ IXIONModsMenu.cs
@@ -1,8 +1,9 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.IL2CPP;
 using BepInEx.Logging;
 using BulwarkStudios.GameSystems.Ui;
-using BulwarkStudios.Stanford;
+using BulwarkStudios.Stanford.Core.GameStates;
 using I2.Loc;
 using System;
 using System.Collections.Generic;
@@ -11,96 +12,171 @@ using System.Text;
 using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-// using ConfigManager.UI;
-using BepInEx.Configuration;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
-namespace IXIONModsMenu {
+namespace Olympia.IXIONModsMenu {
 
-    [BepInPlugin("com.Olympia.IXIONModsMenu", "IXION Mods Menu", "0.1")]
-    public class IXIONModsMenuPlugin : BasePlugin {
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class ModsMenu : BasePlugin {
 
+        public const string GUID = "Olympia.IXIONModsMenu";
+        public const string NAME = "IXION Mods Menu";
+        public const string VERSION = "0.2.0";
+
+        public static Phase CurrentPhase { get; private set; }
+        public static ConfigEntry<string> titleText;
         internal static new ManualLogSource Log;
-        internal static ConfigEntry<string> buttonText;
+
+        [Flags]
+        public enum Phase {
+            None = 0,
+            Init = 1,
+            Splashes = 2,
+            Loading = 4,
+            MainMenu = 8,
+            Interior = 16,
+            ShipView = 32,
+            StarSys = 64,
+            Cutscene = 128,
+            Prologue = 256,
+            Chapter1 = 512,
+            Chapter2 = 1024,
+            Chapter3 = 2048,
+            Chapter4 = 4096,
+            Chapter5 = 8192,
+            Unknown = 16384,
+            AnyTorus = Interior | ShipView,
+            AnyGame = Interior | ShipView | StarSys,
+            AnyLevel = Prologue | Chapter1 | Chapter2 | Chapter3 | Chapter4 | Chapter5
+        }
 
         public override void Load() {
             Log = base.Log;
-            buttonText = Config.Bind("Settings", "Button Text", "Mods", "Text used on the button");
-            AddComponent<IXIONModsMenuMono>();
+            titleText = Config.Bind("Settings", "Title Text", "Mods", "Title text used in buttons and headers");
+            AddComponent<ModsMenuMono>();
         }
 
-        public class IXIONModsMenuMono : MonoBehaviour {
+        public static bool InAnyPhase(Phase phaseFlags) => (CurrentPhase & phaseFlags) != 0;
+        public static bool InAllPhases(Phase phaseFlags) => CurrentPhase.HasFlag(phaseFlags);
 
-            bool uiSetup;
-            bool setupInProgress;
-            GameObject btn;
-            GameObject window;
+        public class ModsMenuMono : MonoBehaviour {
 
-            public void Awake() => DontDestroyOnLoad(gameObject);
-            
-            public void Update() {
-                if (setupInProgress) return;
-                // Log.LogInfo("active: " + SceneManager.GetActiveScene().name + " / is sub: " + SceneManager.GetActiveScene().isSubScene + " / total: " + SceneManager.sceneCount);
+            private readonly Dictionary<string, Phase> sceneLookup = new Dictionary<string, Phase> {
+                ["Initialize"] = Phase.Splashes,
+                ["GameSetup"] = Phase.Loading,
+                ["Chapter0"] = Phase.Prologue,
+                ["Chapter0End"] = Phase.Cutscene
+            };
+            private readonly Dictionary<Game.VIEW, Phase> viewLookup = new Dictionary<Game.VIEW, Phase> {
+                [Game.VIEW.TORUS] = Phase.Interior,
+                [Game.VIEW.SHIP] = Phase.ShipView,
+                [Game.VIEW.SOLAR_SYSTEM] = Phase.StarSys
+            };
+
+            private GameObject btn, window;
+            private List<Tuple<ConfigEntry<bool>, TMP_Dropdown>> disabledConfigs;
+
+            public void Awake() {
+                DontDestroyOnLoad(gameObject);
+                disabledConfigs = new List<Tuple<ConfigEntry<bool>, TMP_Dropdown>>();
+                SceneManager.activeSceneChanged += (UnityAction<Scene, Scene>)OnSceneChanged;
+            }
+
+            private void OnSceneChanged(Scene _, Scene __) {
                 string sceneName = SceneManager.GetActiveScene().name;
-                if (uiSetup && sceneName != "MainMenu") uiSetup = false;
-                if (sceneName != "MainMenu" || uiSetup) return;
-                setupInProgress = true;
-                AddModsBtn();
-                SetupModListUI();
-                setupInProgress = false;
-                uiSetup = true;
+                Log.LogInfo(sceneName);
+                Phase prevPhase = CurrentPhase;
+                CurrentPhase = Phase.None;
+                if (Enum.TryParse(sceneName, out Phase phase))
+                    CurrentPhase = phase;
+                else if (sceneLookup.ContainsKey(sceneName))
+                    CurrentPhase = sceneLookup[sceneName];
+                if (viewLookup.ContainsKey(Game.view))
+                    CurrentPhase |= viewLookup[Game.view];
+                if (CurrentPhase == Phase.None)
+                    CurrentPhase = Phase.Unknown;
+                if (prevPhase == CurrentPhase) return;
+                Log.LogInfo(prevPhase + " -> " + CurrentPhase);
+                if (InAnyPhase(Phase.MainMenu) && btn == null) SetupUI();
             }
 
-            private void AddModsBtn() {
-                Log.LogInfo("Adding Mod Button...");
-                GameObject stnsBtn = GameObject.Find("Button_Settings");
-                btn = Instantiate(stnsBtn, stnsBtn.transform.parent);
-                btn.transform.SetSiblingIndex(3);
-                btn.transform.GetChild(1).GetComponent<Localize>().enabled = false;
-                btn.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = buttonText.Value;
-                btn.GetComponent<UiButtonTriggerUnityEvent>().enabled = false;
-                Action modBtnAction = delegate { ToggleModList(true); }; // UIManager.ShowMenu = !UIManager.ShowMenu;
-                btn.GetComponent<UiButton>().add_OnTriggered(modBtnAction);
-                Log.LogInfo("Button Added");
+            private void Update() {
+                if (!viewLookup.ContainsKey(Game.view)) return;
+                if (InAnyPhase(viewLookup[Game.view])) return;
+                Phase prevPhase = CurrentPhase;
+                CurrentPhase &= ~Phase.AnyGame;
+                CurrentPhase |= viewLookup[Game.view];
+                Log.LogInfo(prevPhase + " -> " + CurrentPhase);
+                if (btn == null) SetupUI();
             }
-            private void SetupModListUI() {
-                Log.LogInfo("Setting Up Mod List...");
-                GameObject loadWindow = GameObject.Find("UI Window Load Game");
-                window = Instantiate(loadWindow, loadWindow.transform.parent);
+
+            private void SetupUI() {
+                GameObject btnTemplate = GameObject.Find(InAnyPhase(Phase.MainMenu) ? "Button_LoadGame" : "Container/Content/Settings");
+                btn = Instantiate(btnTemplate, btnTemplate.transform.parent);
+                btn.transform.SetSiblingIndex(InAnyPhase(Phase.MainMenu) ? 3 : 6);
+                UpdateText(btn.transform.GetChild(1));
+                btn.GetComponent<UiButtonTriggerUnityEvent>().enabled = false;
+                Action modBtnAction = delegate { ToggleModList(true); };
+                btn.GetComponent<UiButton>().add_OnTriggered(modBtnAction);
+
+                GameObject windowTemplate = GameObject.Find("UI Window Load Game");
+                window = Instantiate(windowTemplate, windowTemplate.transform.parent);
                 window.GetComponent<CanvasGroup>().alpha = 1;
                 window.GetComponent<Canvas>().enabled = true;
                 window.GetComponent<GraphicRaycaster>().enabled = true;
-                window.transform.GetChild(0).GetChild(1).GetChild(2).GetComponent<Localize>().enabled = false;
-                window.transform.GetChild(0).GetChild(1).GetChild(2).GetComponent<TextMeshProUGUI>().text = buttonText.Value;
+                UpdateText(window.transform.GetChild(0).GetChild(1).GetChild(2));
                 window.transform.GetChild(0).GetChild(1).GetChild(1).GetComponent<UiButtonTriggerUnityEvent>().enabled = false;
                 Action xBtnAction = delegate { ToggleModList(false); };
                 window.transform.GetChild(0).GetChild(1).GetChild(1).GetComponent<UiButton>().add_OnTriggered(xBtnAction);
                 Transform listParent = window.transform.GetChild(0).GetChild(2).GetChild(0).GetChild(0).transform;
                 GameObject tempCont = GameObject.Find("UI Window Settings").transform.GetChild(0).GetChild(2).GetChild(0).GetChild(0).GetChild(1).gameObject;
                 tempCont.SetActive(true);
-                GameObject entryTemplate = GameObject.Find("Entry_Language");
-                GameObject separator = GameObject.Find("Separator");
-                foreach (BepInPlugin plugin in IL2CPPChainloader.Instance.Plugins.Select(p => p.Value.Metadata)) {
+                GameObject entryTemplate = GameObject.Find("Entry_Tutorial");
+                GameObject separator = GameObject.Find("Game/Separator");
+                foreach (PluginInfo plugin in IL2CPPChainloader.Instance.Plugins.Values.OrderBy(p => p.Metadata.GUID != GUID).ThenBy(p => p.Metadata.Name)) {
                     GameObject listItem = Instantiate(entryTemplate, listParent);
-                    listItem.transform.GetChild(1).gameObject.SetActive(false);
                     listItem.transform.GetChild(0).GetComponent<Localize>().enabled = false;
-                    listItem.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = plugin.Name + " - " + plugin.Version;
+                    listItem.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = plugin.Metadata.Name + " - " + plugin.Metadata.Version;
                     if (separator != null) Instantiate(separator, listParent);
+
+                    TMP_Dropdown dropDown = listItem.transform.GetChild(1).gameObject.GetComponent<TMP_Dropdown>();
+                    if (dropDown == null) continue;
+                    ConfigEntry<bool> configEntry = plugin.Instance.GetType().GetField("disabledInMenu")?.GetValue(plugin.Instance) as ConfigEntry<bool>;
+                    if (configEntry == null) {
+                        dropDown.SetValueWithoutNotify(1);
+                        dropDown.enabled = false;
+                        listItem.transform.GetChild(1).GetChild(1).gameObject.SetActive(false);
+                        continue;
+                    }
+                    dropDown.onValueChanged.AddListener((UnityAction<int>)UpdateModDisabledConfigs);
+                    dropDown.SetValueWithoutNotify(configEntry.Value ? 0 : 1);
+                    disabledConfigs.Add(Tuple.Create(configEntry, dropDown));
                 }
                 tempCont.SetActive(false);
                 window.SetActive(false);
-                Log.LogInfo("Mod List Setup Complete");
+            }
+
+            private void UpdateText(Transform transform) {
+                transform.GetComponent<Localize>().enabled = false;
+                transform.GetComponent<TextMeshProUGUI>().text = titleText.Value;
+            }
+
+            private void UpdateModDisabledConfigs(int _) {
+                foreach (var tuple in disabledConfigs)
+                    if (!tuple.Item1.Value && tuple.Item2.value == 0)
+                        tuple.Item1.BoxedValue = true;
+                    else if (tuple.Item1.Value && tuple.Item2.value == 1)
+                        tuple.Item1.BoxedValue = false;
             }
 
             private void ToggleModList(bool enabled) {
                 window.SetActive(enabled);
             }
 
+
         }
+
     }
-
-    
-
-
 }
